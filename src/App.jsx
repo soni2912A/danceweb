2cefb557905bd3dea67f59828a273a83f61dc842
Soni2912@ <bansodesony48@gmail.com>
2025-11-13 12:52:22 +0530

    navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React from "react";
-import Navbar from "./components/navbar";
-import "./App.css";
+// import React from "react";
+// import Navbar from "./components/navbar";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <Navbar/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <>
+      <Navbar />
+      <Footer />
+      {/* Other sections */}
+    </>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,48 @@
+// import React from "react";
+// import "./navbar.css";
 
+// function Navbar() {
+//   return (
+//     <nav className="navbar">
+//       <div className="nav-left">
+//         <h2 className="logo">Movement Studios</h2>
+//       </div>
+
+//       <div className="nav-center">
+//         <a href="#home" className="nav-link">Home</a>
+//         <a href="#classes" className="nav-link">Classes</a>
+//         <a href="#about" className="nav-link">About</a>
+//       </div>
+
+//       <div className="nav-right">
+//         <p>224 Nostrand Ave, Brooklyn, NY</p>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+
+import React from "react";
+import "./navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="nav- left">
-        <h2 className="logo">Movement Studios.</h2>
+      <div className="nav-left">
+        {/* <a href="#home" className="nav-link">Home</a> */}
+        <a href="#classes" className="nav-link">Classes</a>
+        <a href="#about" className="nav-link">About</a>
+        <p>224 Nostrand Ave, Brooklyn, NY</p>
       </div>
 
       <div className="nav-center">
-        <a href="#home">Home</a>
-        <a href="#classes">Classes</a>
-        <a href="#about">About</a>
+        {/* <p>224 Nostrand Ave, Brooklyn, NY</p> */}
       </div>
 
       <div className="nav-right">
-        <p>224 Nostrand Ave, Brooklyn, NY</p>
+        <h2 className="logo">Movement  Studios</h2>
       </div>
     </nav>
   );
