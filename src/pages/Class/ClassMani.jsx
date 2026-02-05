2319225d61f00152690c197d73beebeb84c3bfa3
Soni2912@ <bansodesony48@gmail.com>
2026-02-05 12:49:13 +0530

    responsive both pages

--- src/pages/Class/ClassMani.jsx
+++ src/pages/Class/ClassMani.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./class.css";
 import ClassesImage from "../../assets/images/classes1.png";
 import Team2 from "../../components/Team2/team2";
-import TickerBar from "../../components/Middle-section/TickerBar"
+// import TickerBar from "../../components/Middle-section/TickerBar"
 import CTA from "../../components/Middle-section/CTASection"
 
 const ClassMani = () => {
@@ -15,10 +15,10 @@ const ClassMani = () => {
         <h1 className="classes-title">Classes</h1>
       </div>
     </section>
-    <TickerBar/>
+    
     
           <Team2 />
-          <CTA/>
+          
           </>
 
   );
